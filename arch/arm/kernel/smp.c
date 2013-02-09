ae1e784dda3b9aa28edc16f169f9e0c30abd6676
Snuzzo <jrracinfan@gmail.com>
2013-02-08 21:16:06 -0600

    [PATCH] ARM Topology by Linaro team allows individual  queue'ing of tasks to cores by the scheduler
    Conflicts:
    	arch/arm/Kconfig
    	arch/arm/include/asm/topology.h
    	arch/arm/kernel/Makefile
    	arch/arm/kernel/smp.c
    	arch/arm/kernel/topology.c

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -33,6 +33,9 @@
 #include <asm/cputype.h>
 #include <asm/exception.h>
 #include <asm/idmap.h>
+#ifdef CONFIG_ARM_CPU_TOPOLOGY
+#include <asm/topology.h>
+#endif
 #include <asm/topology.h>
 #include <asm/mmu_context.h>
 #include <asm/pgtable.h>
@@ -314,7 +317,9 @@ void __ref cpu_die(void)
 static void __cpuinit smp_store_cpu_info(unsigned int cpuid)
 {
 	struct cpuinfo_arm *cpu_info = &per_cpu(cpu_data, cpuid);
-
+#ifdef CONFIG_ARM_CPU_TOPOLOGY
+	store_cpu_topology(cpuid);
+#endif	
 	cpu_info->loops_per_jiffy = loops_per_jiffy;
 
 	store_cpu_topology(cpuid);
@@ -411,9 +416,9 @@ void __init smp_prepare_cpus(unsigned int max_cpus)
 {
 	unsigned int ncores = num_possible_cpus();
 
+#ifdef CONFIG_ARM_CPU_TOPOLOGY
 	init_cpu_topology();
-
-	smp_store_cpu_info(smp_processor_id());
+#endif
 
 	/*
 	 * are we trying to boot more cores than exist?
