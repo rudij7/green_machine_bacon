e11a241ce8d247bcc9dbd7914d8144525311cc1d
anarkia1976 <stefano.villa1976@gmail.com>
2014-12-28 16:34:19 +0100

    mach-msm: cpufreq: fix compile errors without MSM_CPU_FREQ_SET_MIN_MAX enabled
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -208,11 +208,14 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 	policy->cur = table[index].frequency;
 #endif
 	cpufreq_frequency_table_get_attr(table, policy->cpu);
+
+
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-	/* set safe default min and max speeds */
-	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
-	policy->min = CONFIG_MSM_CPU_FREQ_MIN;
+        /* set safe default min and max speeds */
+        policy->max = CONFIG_MSM_CPU_FREQ_MAX;
+        policy->min = CONFIG_MSM_CPU_FREQ_MIN;
 #endif
+
 	return 0;
 }
 
